ed493e46e0172109514fd0663b5b17419e234666
Uehara.Keizo <Uehara.Keizo@mss.co.jp>
2019-06-12 11:33:00 +0900

    Implement UmlsConverter

--- targetmine-bio-sources/umls/src/main/java/org/intermine/bio/dataconversion/UmlsConverter.java
+++ targetmine-bio-sources/umls/src/main/java/org/intermine/bio/dataconversion/UmlsConverter.java
@@ -11,9 +11,11 @@ package org.intermine.bio.dataconversion;
  */
 
 import java.io.Reader;
+import java.util.Iterator;
 
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
+import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
 
 
@@ -24,8 +26,8 @@ import org.intermine.xml.full.Item;
 public class UmlsConverter extends BioFileConverter
 {
     //
-    private static final String DATASET_TITLE = "Add DataSet.title here";
-    private static final String DATA_SOURCE_NAME = "Add DataSource.name here";
+    private static final String DATASET_TITLE = "UMLS";
+    private static final String DATA_SOURCE_NAME = "UMLS";
 
     /**
      * Constructor
@@ -42,6 +44,13 @@ public class UmlsConverter extends BioFileConverter
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
-	
+    	Iterator<String[]> iterator = FormattedTextParser.parseDelimitedReader(reader,'|');
+    	while(iterator.hasNext()) {
+    		String[] mrConsoRow  = iterator.next();
+    		Item item = createItem("IntegratedTerm");
+    		item.setAttribute("identifier", mrConsoRow [0]);
+    		item.setAttribute("name", mrConsoRow [14]);
+    		store(item);
+    	}
     }
 }
