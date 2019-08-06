225aca18e913500eb6a518c716445a5328da3e14
Kubota.Hiroshi <Kubota.Hiroshi.f@cbo.mss.co.jp>
2019-08-06 16:53:16 +0900

    hgmd converter commit dev code.

--- targetmine-bio-sources/hgmd/src/main/java/org/intermine/bio/dataconversion/HgmdConverter.java
+++ targetmine-bio-sources/hgmd/src/main/java/org/intermine/bio/dataconversion/HgmdConverter.java
@@ -50,30 +50,136 @@ public class HgmdConverter extends BioDBConverter
 
         Connection connection = getDatabase().getConnection();
 
-        // process data with direct SQL queries on the source database, for example:
-        
-        // Statement stmt = connection.createStatement();
-        // String query = "select column from table;";
-        // ResultSet res = stmt.executeQuery(query);
-        // while (res.next()) {
-        // }
         Statement stmt = connection.createStatement();
-        String queryAllmut = "select disease, gene, phen_id from hgmd_pro.allmut JOIN " +
-                "hgmd_phenbase.hgmd_mutation ON hgmd_pro.allmut.acc_num = hgmd_phenbase.hgmd_mutation.acc_num limit 5;";
+
+        // TODO: SQL 要確認
+        String queryAllmut = "select * from hgmd_pro.allmut " +
+                "LEFT JOIN hgmd_pro.mutnomen ON " +
+                "hgmd_pro.allmut.acc_num = hgmd_pro.mutnomen.acc_num "
+                "LEFT JOIN hgmd_phenbase.hgmd_mutation ON " +
+                "hgmd_pro.allmut.acc_num = hgmd_phenbase.hgmd_mutation.acc_num " +
+                "LEFT JOIN hgmd_phenbase.phenotype_concept ON " +
+                "hgmd_phenbase.hgmd_mutation.phen_id = hgmd_phenbase.phenotype_concept.phen_id " +
+                // TODO : debug
+                "limit 5 " +
+                        ";";
         ResultSet resAllmut = stmt.executeQuery(queryAllmut);
         while (resAllmut.next()) {
-            String disease = resAllmut.getString("disease");
-            String gene = resAllmut.getString("gene");
-            String phenId = resAllmut.getString("phen_id");
 
-            Item item = createItem("Hgmd");
-            item.setAttribute("disease", disease);
-            item.setAttribute("gene", gene);
-            item.setAttribute("phenId", phenId);
-            store(item);
+            createHgmd(resAllmut);
+            createPublication(resAllmut);
+
 
         }
     }
+
+    private void createHgmd(ResultSet response) {
+        String identifer = response.getString("acc_num");
+        String describe = resAllmut.getString("descr");
+        String variantClass = resAllmut.getString("tag");
+
+        Item item = createItem("Hgmd");
+        item.setAttribute("identifier", identifer);
+        item.setAttribute("describe", describe);
+        item.setAttribute("variantClass", variantClass);
+        store(item);
+    }
+
+    private void createPublication(ResultSet response) {
+        String pubMedId = response.getString("pmid");
+
+        Item item = createItem("Publication");
+        item.setAttribute("pubMedId", pubMedId);
+        store(item);
+    }
+
+    private void createSnp(ResultSet response) {
+        String identifier = response.getString("dbsnp");
+        if(identifier == null || identifier.length() == 0) {
+            identifier = response.getString("acc_num");
+        }
+
+        String coodStart = response.getString("coordSTART");
+        String chromosome = response.getString("chromosome");
+        // TODO: データの作り方 要確認 : allmut.chromosomeとallmut.coordSTART
+        String location = chromosome + coodStart;
+
+        // TODO: データの作り方 要確認 : allmut.hgvsまたはallmut.deletionまたはallmut.insertion
+        String refSnpAllele;
+        if(response.getString("hgvs") != null && response.getString("hgvs").length() != 0) {
+            refSnpAllele = response.getString("hgvs");
+        }else if(response.getString("deletion") != null && response.getString("deletion").length() != 0) {
+            refSnpAllele = response.getString("deletion");
+        }else if(response.getString("insertion") != null && response.getString("insertion").length() != 0) {
+            refSnpAllele = response.getString("insertion");
+        }
+
+        // TODO: データの作り方　要確認 :  ?
+        String orientation;
+
+        Item item = createItem("SNP");
+        item.setAttribute("identifier", identifier);
+        if (!StringUtils.isEmpty(location)) {
+            item.setAttribute("location", location);
+        }
+        if (!StringUtils.isEmpty(chromosome)) {
+            item.setAttribute("chromosome", chromosome);
+        }
+        if (!StringUtils.isEmpty(refSnpAllele)) {
+            item.setAttribute("refSnpAllele", refSnpAllele);
+        }
+        if (!StringUtils.isEmpty(orientation)) {
+            item.setAttribute("orientation", orientation);
+        }
+
+        store(item);
+    }
+
+    private void createGene(ResultSet response) {
+        // refCore
+        String refCore = response.getString("refCORE");
+        // TODO : refCoreを変換？
+        String ncbiGeneId = refCore;
+
+        Item item = createItem("Gene");
+        item.setAttribute("ncbiGeneId", ncbiGeneId);
+        store(item);
+    }
+
+//    private String createSNPReference(String mrnaAcc, String mrnaPos, String orientation,
+//                                      String allele, String codon, String proteinAcc, int aaPos, String residue,
+//                                      String funcRef, String vaItemRef) throws ObjectStoreException {
+//        Item item = createItem("SNPReference");
+//        item.setAttribute("mrnaAccession", mrnaAcc);
+//        if (!StringUtils.isEmpty(mrnaPos)) {
+//            item.setAttribute("mrnaPosition", mrnaPos);
+//        }
+//        if (!StringUtils.isEmpty(orientation)) {
+//            item.setAttribute("orientation", orientation);
+//        }
+//        if (!StringUtils.isEmpty(allele)) {
+//            item.setAttribute("mrnaAllele", allele);
+//        }
+//        if (!StringUtils.isEmpty(codon)) {
+//            item.setAttribute("mrnaCodon", codon);
+//        }
+//        if (!StringUtils.isEmpty(proteinAcc)) {
+//            item.setAttribute("proteinAccession", proteinAcc);
+//        }
+//        if (aaPos > 0) {
+//            item.setAttribute("proteinPosition", String.valueOf(aaPos));
+//        }
+//        if (!StringUtils.isEmpty(residue)) {
+//            item.setAttribute("residue", residue);
+//        }
+//        if (funcRef != null) {
+//            item.setReference("function", funcRef);
+//        }
+//        item.setReference("annotation", vaItemRef);
+//        store(item);
+//
+//        return item.getIdentifier();
+//    }
 
     /**
      * {@inheritDoc}
